62e7a15749798d90a3be0dd1636af847b5b0f8ff
Krishna-windows <krshnabiswal619@gmail.com>
2024-07-01 17:59:56 +0530

    chore : removed firestore

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
@@ -26,4 +25,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const firestore = getFirestore(app);
