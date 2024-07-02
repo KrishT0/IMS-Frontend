1473aa50d837ec9ae289c98df71ef74a3f27a48e
Krishna-windows <krshnabiswal619@gmail.com>
2024-07-02 21:48:52 +0530

    feat : added user store(zustand)

---
+++ src/store/user.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { user } from "../types";
+
+type userType = {
+  user: user | null;
+  setUser: (user: user | null) => void;
+  logout: () => void;
+  setToken: (token: string) => void;
+};
+
+export const useUser = create<userType>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      logout: () => set({ user: null }),
+      setToken: (token: string) =>
+        set((state) => {
+          if (state.user) {
+            return { user: { ...state.user, token } };
+          }
+          return state;
+        }),
+    }),
+    { name: "user" }
+  )
+);
