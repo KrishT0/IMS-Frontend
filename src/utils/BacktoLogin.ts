a055d189f8db219175755dbd7ef83c21c04b5b2c
Krishna-windows <krshnabiswal619@gmail.com>
2024-07-28 17:11:19 +0530

    feat(utils) : added getting back to login page util

---
+++ src/utils/BacktoLogin.ts
@@ -0,0 +1,17 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+
+/**
+ * 
+ * @description This function will sign out the user and redirect the user to the login page.
+ */
+export const BackToLogin = () => {
+  signOut(auth)
+    .then(() => {
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return null;
+};
