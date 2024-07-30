511d15677063ef8b731140c8973bee99583f0a28
Krishna-windows <krshnabiswal619@gmail.com>
2024-07-30 20:41:51 +0530

    chore(util) : changed getting new accesstoken logic

--- src/utils/AceessToken.ts
+++ src/utils/AceessToken.ts
@@ -1,16 +1,25 @@
-import { requestingNewAccessToken } from "../api/index";
+// import { requestingNewAccessToken } from "../api/index";
 import { useUser } from "../store/user";
+import { auth } from "../firebase/firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
 
 /**
  *
- * @description Function to get a new access token using refresh token.
+ * @description Function to get a new access token.
  */
-export const getNewAccessToken = async () => {
-  try {
-    const response = await requestingNewAccessToken();
-    console.log(response);
-    useUser.getState().setToken(response.accessToken);
-  } catch (error) {
-    console.log(error);
-  }
+export const getNewAccessToken = async (API: any) => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      try {
+        const refreshedIdToken = await user.getIdToken(true);
+        console.log("Refreshed ID Token:", refreshedIdToken);
+        useUser.getState().setToken(refreshedIdToken);
+        API();
+      } catch (error) {
+        console.error("Error refreshing token:", error);
+      }
+    } else {
+      console.log("No user is signed in.");
+    }
+  });
 };
